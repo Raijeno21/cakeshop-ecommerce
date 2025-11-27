d0137865358a90f0331e99ad9659ba0aa830101e
Raijeno21 <jenopogi221@gmail.com>
2025-11-27 22:09:39 +0800

    created a navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,7 @@
 import Link from "next/link";
 
 const Home = () => {
-  return (
-    <main className="flex gap-10 bg-red-400">
-      <Link href="/checkout">Checkout</Link>
-      <Link href="/shop">shop</Link>
-      <Link href="/carts">carts</Link>
-      <Link href="/wishlist">wishlist</Link>
-    </main>
-  );
+  return <></>;
 };
 
 export default Home;
---
+++ src/components/PhoneNav.tsx
@@ -0,0 +1,36 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { icon } from "../svgIcons";
+const PhoneNav = () => {
+  const navigation = [
+    { name: "Shop", icon: icon.shop, link: "/shop" },
+    { name: "Explore", icon: icon.searchIcon, link: "/explore" },
+    { name: "Cart", icon: icon.cart, link: "/carts" },
+    { name: "Favorite", icon: icon.heart, link: "/favorite" },
+    { name: "Account", icon: icon.user, link: "/account" },
+  ];
+  const location = usePathname();
+  console.log(location);
+  return (
+    <nav className="w-full h-16 text-sm fixed bottom-0 flex justify-between px-5 rounded-t-xl bg-gray-200 shadow">
+      {navigation.map((nav, i) => (
+        <div
+          className={`${
+            location === nav.link
+              ? "bg-pink-500  text-white rounded-sm transition duration-300 ease-in-out "
+              : ""
+          }flex flex-1 items-center justify-center`}
+          key={i}
+        >
+          <Link className=" flex-1" href={nav.link}>
+            <div className="w-10  aspect-square mx-auto p-1"> {nav.icon}</div>
+            <div className="flex justify-center">{nav.name}</div>
+          </Link>
+        </div>
+      ))}
+    </nav>
+  );
+};
+
+export default PhoneNav;
